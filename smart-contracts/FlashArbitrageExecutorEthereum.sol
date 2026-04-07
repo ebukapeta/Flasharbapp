775b2bf05d9f91875e6393fbde6ffe56e05126e1
ebukapeta <ucpeter10@yahoo.com>
2026-04-07 13:52:02 +0100

    Add files via upload

---
+++ smart-contracts/FlashArbitrageExecutorEthereum.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../FlashArbitrageExecutor.sol";
+
+// Ethereum deployment entrypoint. Use this contract for Sepolia/mainnet deployments.
+contract FlashArbitrageExecutorEthereum is FlashArbitrageExecutor {}
